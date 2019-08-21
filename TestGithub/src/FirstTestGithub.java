640cb32977e1865f1b9d8420b1e6bee5c790d11c
Asus <Asus@DESKTOP-D9Q8J6U>
2019-08-21 21:29:40 +0400

    Moving forward with Github

---
+++ TestGithub/src/FirstTestGithub.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Asus
+ *
+ */
+public class FirstTestGithub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

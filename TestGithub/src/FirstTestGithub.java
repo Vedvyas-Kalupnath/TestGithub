a2ea7eb236c09bd888df7ec4d36f5cf8cb114cfd
Asus <Asus@DESKTOP-D9Q8J6U>
2019-08-21 21:30:23 +0400

    Moving forward with Github

--- TestGithub/src/FirstTestGithub.java
+++ TestGithub/src/FirstTestGithub.java
@@ -13,6 +13,9 @@ public class FirstTestGithub {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+		System.out.println("Today is a beatiful day.");
 
 	}
 
